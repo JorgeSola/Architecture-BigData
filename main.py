8060d06efe3f1769094e5c72801bfbe30391957f
jorgeSolaAsensio <kokesola.94@gmail.com>
2020-09-18 18:11:29 +0200

    version with arguments

--- main.py
+++ main.py
@@ -15,12 +15,53 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 def init_postgresql(config):
+    logger.info(f'Starting connection with database...')
     global conn 
     conn = psycopg2.connect(
                 host = config['postgresql_host'],
                 database = config['postgresql_db'],
                 user = config['postgresql_user'],
                 password = config['postgresql_password'])
+
+def check_tables_postgresql():
+    global conn
+    cursor = conn.cursor()
+    cursor.execute("select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';")
+    result = [list(i) for i in cursor.fetchall()]
+    
+    return result
+
+def airbnb_postgresql_process(table):
+    global conn
+
+    cursor = conn.cursor()
+    if table == []:
+        sentence_1 = f"""create table public.airbnb_listings(id int4 primary key not null,host_id int4 not null,host_name varchar(100),host_neighbourdhood text,host_listings_count int4,city varchar(100),country varchar(50),property_type varchar(50),room_type varchar (50),price int4,security_deposit int4, 	cleaning_fee int4, 	number_reviews int4, 	review_score int4, 	cancellation_policy varchar(30));"""
+        cursor.execute(sentence_1)
+
+        sentence_2 = f"""create table public.flat_information( 	host_id int4 not null primary key, 	tv int4, 	cable_tv int4, 	kitchen int4, 	smoking_allowed int4, 	pets_allowed int4, 	heating int4, 	washer int4, 	dryer int4, 	checkin_24h int4);"""
+        cursor.execute(sentence_2)
+
+        sentence_3 = f"""create table public.airbnb_secondary_information( host_id int4 not null primary key, street text, zipcode int4, country_code varchar(4), latitude float, longitud float, bedrooms int4, beds int, weekly_price int4, monthly_price int4, minimun_nights int4, review_score_accuracy int4, review_score_cleanliness int4, review_score_checkin int4, review_score_communitcation int4, review_score_location int4 );"""
+        cursor.execute(sentence_3)
+
+        conn.commit()
+
+    if table != []:
+        sentence_1 = f"""create table public.airbnb_listings_temp(id int4 primary key not null,host_id int4 not null,host_name varchar(100),host_neighbourdhood text,host_listings_count int4,city varchar(100),country varchar(50),property_type varchar(50),room_type varchar (50),price int4,security_deposit int4, 	cleaning_fee int4, 	number_reviews int4, 	review_score int4, 	cancellation_policy varchar(30));"""
+        cursor.execute(sentence_1)
+
+        sentence_2 = f"""create table public.flat_information_temp(host_id int4 not null primary key, 	tv int4, 	cable_tv int4, 	kitchen int4, 	smoking_allowed int4, 	pets_allowed int4, 	heating int4, 	washer int4, 	dryer int4, 	checkin_24h int4);"""
+        cursor.execute(sentence_2)
+
+        sentence_3 = f"""create table public.airbnb_secondary_information_temp( host_id int4 not null primary key, street text, zipcode int4, country_code varchar(4), latitude float, longitud float, bedrooms int4, beds int, weekly_price int4, monthly_price int4, minimun_nights int4, review_score_accuracy int4, review_score_cleanliness int4, review_score_checkin int4, review_score_communitcation int4, review_score_location int4 );"""
+        cursor.execute(sentence_3)
+
+        #INSERT DATA IN NEW TEMP TABLES
+        conn.commit()
+
+
+
 
 def download_airbnb_dataset_csv():
 
@@ -35,6 +76,41 @@ def download_airbnb_dataset_csv():
     t1 = time.time()
     logger.info(f"Download time: {round(t1 - t0, 2)}")
 
+def save_data_postgresql(data, table):
+
+    t0 = time.time()
+    logger.info(f'Process {table} data....' )
+    data = data.dropna(how='any',axis=0) 
+    data = data.to_dict(orient = 'records')
+    cursor = conn.cursor()
+    columns = [str(column).lower().replace(' ','_') for column in data[0].keys()]
+    columns =  ",".join(columns)
+    if table == 'flat_information':
+        columns = columns.replace('24-hour_check-in','checkin_24h')
+    new_data = []
+    for ix, row in enumerate(data):
+        block = []
+        block = [str(value).replace("'",'') for value in row.values()]
+        block = str(block).replace('[','').replace(']','')
+        new_data.append(block)
+        if ix > 1 and ix % 5000 == 0:
+            values = "),(".join(new_data)
+            sentence = f"INSERT INTO {table} ({columns}) VALUES ({values}) ON CONFLICT DO NOTHING" 
+            cursor.execute(sentence)
+            conn.commit()
+            new_data = []
+
+    logger.info(f'Total files processed: {ix}')
+    values = "),(".join(new_data)
+    sentence = f"INSERT INTO {table} ({columns}) VALUES ({values}) ON CONFLICT DO NOTHING" 
+    cursor.execute(sentence)
+    conn.commit()
+    new_data = []
+    logger.info(f'{table} finished')
+    t1 = time.time()
+    logger.info(f'Total time procese: {t1 - t0}')
+
+
 def create_csv_files():
 
     logger.info(f"Getting the main information....")
@@ -42,17 +118,21 @@ def create_csv_files():
 
     df = pd.read_csv('airbnb_listings.csv',sep= ';', encoding = 'utf-8',error_bad_lines=False, index_col=False, dtype='unicode',low_memory=False,  lineterminator='\n')
 
-    listening_info = df[['ID', 'Host ID', 'Host Name', 'Host Neighbourhood', 'Host Listings Count', 'City', 'Country', 'Property Type', 'Room Type', 'Price', 'Security Deposit', 'Cleaning Fee', 'Number of Reviews', 'Review Scores Value', 'Cancellation Policy']]
-    listening_info = listening_info.drop_duplicates(keep='last')
-    flat_info = df[['Host ID','Amenities']]
-    flat_info = flat_info.drop_duplicates(keep='last')
-    secondary_info = df[['Host ID','Street', 'Zipcode', 'Country Code', 'Latitude', 'Longitude', 'Bedrooms', 'Beds', 'Weekly Price', 'Monthly Price', 'Minimum Nights', 'Review Scores Accuracy', 'Review Scores Cleanliness', 'Review Scores Checkin', 'Review Scores Communication', 'Review Scores Location']]
-    
+    airbnb_listings = df[['ID', 'Host ID', 'Host Name', 'Host Neighbourhood', 'Host Listings Count', 'City', 'Country', 'Property Type', 'Room Type', 'Price', 'Security Deposit', 'Cleaning Fee', 'Number of Reviews', 'Review Scores Value', 'Cancellation Policy']]
+    airbnb_listings = airbnb_listings.drop_duplicates(keep='last')
+    airbnb_listings.to_csv('airbnb_listings_mod.csv', index = False, sep = ';')
+    save_data_postgresql(airbnb_listings, 'airbnb_listings')
+
+    flat_information = df[['Host ID','Amenities']]
+    flat_information = flat_information.drop_duplicates(keep='last')
+    process_info(flat_information) #Similar to create csv.
+
+
+    airbnb_secondary_information = df[['Host ID','Street', 'Zipcode', 'Country Code', 'Latitude', 'Longitude', 'Bedrooms', 'Beds', 'Weekly Price', 'Monthly Price', 'Minimum Nights', 'Review Scores Accuracy', 'Review Scores Cleanliness', 'Review Scores Checkin', 'Review Scores Communication', 'Review Scores Location']]
+    airbnb_secondary_information.to_csv('airbnb_secondary_information.csv', index = False, sep = ';')
+    save_data_postgresql(airbnb_secondary_information,'airbnb_secondary_information')
     logger.info(f"Creating the news csv files....")
 
-    listening_info.to_csv('listening_info.csv', index = False, sep = ';')
-    process_info(flat_info)
-    secondary_info.to_csv('secondary_info.csv', index = False, sep = ';')
 
     logger.info(f"News csv files created.")
     t1= time.time()
@@ -67,7 +147,7 @@ def upload_google_storage_connection():
 
     bucket = client_storage.get_bucket('keepcoding_bucket')
 
-    for file in ['listening_info.csv','flat_info.csv','secondary_info.csv']:
+    for file in ['airbnb_listings_mod.csv','flat_information.csv','airbnb_secondary_information.csv']:
         blob = bucket.blob(f'{file}')
         blob.upload_from_string('this is test content!')
         blob.upload_from_filename(filename=f'{file}')
@@ -86,7 +166,7 @@ def upload_google_storage_connection():
 def check_old_files_google_storage():
     
     t0 = time.time()
-    csv_files = ['flat_info.csv','listening_info.csv','secondary_info.csv']
+    csv_files = ['flat_information.csv','airbnb_listings_mod.csv','airbnb_secondary_information.csv']
     logger.info(f"Checking old .csv in the bucket....")
     client_storage = storage.Client()
 
@@ -106,9 +186,9 @@ def check_old_files_google_storage():
 def delete_local_csv():
     try:
         os.remove('airbnb_listings.csv')
-        os.remove('listening_info.csv')
-        os.remove('flat_info.csv')
-        os.remove('secondary_info.csv')
+        os.remove('airbnb_listings_mod.csv')
+        os.remove('flat_information.csv')
+        os.remove('airbnb_secondary_information.csv')
     except:
         print(f'There are not old files')
 
@@ -167,10 +247,12 @@ def process_info(df):
 
     df = pd.DataFrame(new_data)
     df = df.drop_duplicates(keep='last')
-    df.to_csv('flat_info.csv', index = False, sep = ';')
+    df.to_csv('flat_information.csv', index = False, sep = ';')
+    save_data_postgresql(df,'flat_information')
 
 def main():
     
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--postgresql-host', help='', required=True)
     parser.add_argument('--postgresql-user', help='', required=True)
@@ -185,14 +267,20 @@ def main():
               'postgresql_password': args.postgresql_password,
               'postgresql_db': args.postgresql_db}
 
+    t0 = time.time()
     init_postgresql(config)
+    #check_tables_postgresql()
     download_airbnb_dataset_csv()
     create_csv_files()
     check_old_files_google_storage()
     upload_google_storage_connection()
     delete_local_csv()
+    conn.close()
 
-if __name__ == "__main__":
- 
+    logger.info(f'Connection with database closed.')
+    t1 = time.time()
+    logger.info(f'TOTAL PROCESS TIME: {t1 - t0}')
+
+if __name__ == "__main__": 
     main()
     
